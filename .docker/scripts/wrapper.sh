46bce2665a0f32f5302df64c50bd249edf3e6a4b
David Dierickx <diericd@factry.io>
2023-02-07 14:52:35 +0100

    Added dockerfile for debug docker

---
+++ .docker/scripts/wrapper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+start_delve(){
+  while true;
+  do
+    PID=`pgrep gpx_factry`
+    if [[ ! -z "$PID" ]]
+    then
+      /root/go/bin/dlv attach --api-version=2 --headless --listen=:2345 $PID
+    else
+      sleep 1
+    fi
+  done
+}
+
+start_delve &
+
+/run.sh
