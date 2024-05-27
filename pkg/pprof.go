03e568db05326fb7de8859ff81da608676b5e1d3
David Dierickx <diericd@factry.io>
2024-05-27 18:46:52 +0200

    Only use pprof on specific build tags
      * Problem: It is not permitted to access environment variables from plugins. PPROF_ENDPOINT is not an accessible variable.
    
        Details: Code rule violation found in /tmp/validator3055756003/pkg/main.go at line 27

--- pkg/pprof.go
+++ pkg/pprof.go
@@ -1,1 +1,15 @@
+//go:build pprof
+// +build pprof
+
 package main
+
+func init() {
+	go func (){
+		server := &http.Server{
+			Addr:         ":1234",
+			ReadTimeout:  10 * time.Second,
+			WriteTimeout: 10 * time.Second,
+		}
+		_ = server.ListenAndServe()
+	} 
+}
